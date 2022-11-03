2f34944985a13d9db92eb47c3bad577c8debb023
nikitha0691 <116497627+nikitha0691@users.noreply.github.com>
2022-11-03 20:12:22 +0530

    Add files via upload

---
+++ junit/Activity2.java
@@ -0,0 +1,25 @@
+package junit_activities;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class Activity2 {
+
+    @Test
+    public void notEnoughFunds(){
+        BankAccount bankAccount = new BankAccount(9);
+        assertThrows(NotEnoughFundsException.class, () ->bankAccount.withdraw(10));
+
+    }
+
+    @Test
+    public void enoughFunds(){
+        BankAccount bankAccount = new BankAccount(100);
+        assertDoesNotThrow( () -> bankAccount.withdraw(100));
+
+    }
+}
